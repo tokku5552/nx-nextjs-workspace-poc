e3ce5a68d4796d453f272d80054abd73b806b245
tokku5552 <physics.0121@gmail.com>
2022-12-31 00:41:25 +0900

    add: sample cdk stack

--- apps/sample-app-cdk/src/stacks/app-stack.ts
+++ apps/sample-app-cdk/src/stacks/app-stack.ts
@@ -1,9 +1,19 @@
-import { Stack, App, StackProps } from 'aws-cdk-lib';
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class AppStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // defines your stack here
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      bucketName: 'NextAppBucket',
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new s3deploy.BucketDeployment(this, 'BucketDeploy', {
+      sources: [s3deploy.Source.asset('../../../../sample-app/.next')],
+      destinationBucket: bucket,
+    });
   }
 }
